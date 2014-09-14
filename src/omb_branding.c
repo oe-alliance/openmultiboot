ed0b972831177c4fcdf42ff470f4ea4136c212e2
Sandro Cavazzoni <sandro@skanetwork.com>
2014-09-14 16:17:59 +0200

    Add support for custom labels

--- src/omb_branding.c
+++ src/omb_branding.c
@@ -27,6 +27,7 @@
 #include "omb_common.h"
 #include "omb_log.h"
 #include "omb_branding.h"
+#include "omb_utils.h"
 
 omb_device_item *omb_branding_read_info(const char* base_dir, const char *identifier)
 {
@@ -34,46 +35,63 @@ omb_device_item *omb_branding_read_info(const char* base_dir, const char *identi
 	char name[255];
 	char distro_cmd[512];
 	char version_cmd[512];
+	char settings_key[255];
+	char *settings_value;
 
-	version[0] = name[0] = '\0';
+	sprintf(settings_key, "label_%s", identifier);
+	settings_value = omb_utils_read(settings_key);
 
-	sprintf(distro_cmd, "%s %s %s/usr/lib/enigma2/python image_distro", OMB_PYTHON_BIN, OMB_BRANDING_HELPER_BIN, base_dir);
-	FILE *fd = popen(distro_cmd, "r");
-	if (fd) {
-		char buffer[255];
-		char *line = fgets(buffer, sizeof(buffer), fd);
-		if (line) {
-			strtok(line, "\n");
-			strncpy(name, line, sizeof(name));
-		}
-		pclose(fd);
-	}
-	
-	sprintf(version_cmd, "%s %s %s/usr/lib/enigma2/python image_version", OMB_PYTHON_BIN, OMB_BRANDING_HELPER_BIN, base_dir);
-	fd = popen(version_cmd, "r");
-	if (fd) {
-		char buffer[255];
-		char *line = fgets(buffer, sizeof(buffer), fd);
-		if (line) {
-			strtok(line, "\n");
-			strncpy(version, line, sizeof(version));
-		}
-		pclose(fd);
-	}
-	
-	if (strlen(name) == 0)
-		strcpy(name, identifier);
-		
 	omb_device_item *item = malloc(sizeof(omb_device_item));
-	item->label = malloc(strlen(name) + strlen(version) + 10);
 	item->directory = malloc(strlen(base_dir) + 1);
 	item->identifier = malloc(strlen(identifier) + 1);
 	item->next = NULL;
-	if (strcmp(identifier, "flash") == 0)
-		sprintf(item->label, "%s %s (flash)", name, version);
-	else
-		sprintf(item->label, "%s %s", name, version);
 	strcpy(item->directory, base_dir);
 	strcpy(item->identifier, identifier);
+	
+	if (!settings_value) {
+		version[0] = name[0] = '\0';
+
+		sprintf(distro_cmd, "%s %s %s/usr/lib/enigma2/python image_distro", OMB_PYTHON_BIN, OMB_BRANDING_HELPER_BIN, base_dir);
+		FILE *fd = popen(distro_cmd, "r");
+		if (fd) {
+			char buffer[255];
+			char *line = fgets(buffer, sizeof(buffer), fd);
+			if (line) {
+				strtok(line, "\n");
+				strncpy(name, line, sizeof(name));
+			}
+			pclose(fd);
+		}
+	
+		sprintf(version_cmd, "%s %s %s/usr/lib/enigma2/python image_version", OMB_PYTHON_BIN, OMB_BRANDING_HELPER_BIN, base_dir);
+		fd = popen(version_cmd, "r");
+		if (fd) {
+			char buffer[255];
+			char *line = fgets(buffer, sizeof(buffer), fd);
+			if (line) {
+				strtok(line, "\n");
+				strncpy(version, line, sizeof(version));
+			}
+			pclose(fd);
+		}
+	
+		if (strlen(name) == 0)
+			strcpy(name, identifier);
+		
+		item->label = malloc(strlen(name) + strlen(version) + 10);
+		if (strcmp(identifier, "flash") == 0)
+			sprintf(item->label, "%s %s (flash)", name, version);
+		else
+			sprintf(item->label, "%s %s", name, version);
+	}
+	else {
+		item->label = malloc(strlen(settings_value) + 9);
+		if (strcmp(identifier, "flash") == 0)
+			sprintf(item->label, "%s (flash)", settings_value);
+		else
+			sprintf(item->label, "%s", settings_value);
+		free(settings_value)
+	}
+		
 	return item;
 }
